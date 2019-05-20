55f809cbab55a66f71b29feaeb73b57dbf31cae2
Jonas Van Malder <jonas.vanmalder@openanalytics.eu>
2019-05-20 12:59:45 +0200

    fixed license headers, findbugs is now spotbugs

--- webapp/src/main/java/eu/openanalytics/rpooli/api/NodesResource.java
+++ webapp/src/main/java/eu/openanalytics/rpooli/api/NodesResource.java
@@ -1,19 +1,19 @@
-/** This file is part of RPooli.
-*
-* RPooli is free software: you can redistribute it and/or modify
-* it under the terms of the Apache License as published by
-* The Apache Software Foundation, either version 2 of the License, or
-* (at your option) any later version.
-* 
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* Apache License for more details.
-* 
-* You should have received a copy of the Apache License
-* along with RPooli.  If not, see <http://www.apache.org/licenses/>
-*/
-
+/**
+ * This file is part of RPooli.
+ *
+ * RPooli is free software: you can redistribute it and/or modify
+ * it under the terms of the Apache License as published by
+ * The Apache Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Apache License for more details.
+ *
+ * You should have received a copy of the Apache License
+ * along with RPooli.  If not, see <http://www.apache.org/licenses/>.
+ */
 package eu.openanalytics.rpooli.api;
 
 import static eu.openanalytics.rpooli.api.spec.resource.Nodes.GetNodesByNodeIdResponse.withJsonOK;
